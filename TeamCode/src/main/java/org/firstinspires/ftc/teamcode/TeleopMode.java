8afc81a4fa55cfbd7972af557ce031934d6ed245
Rene-Baquero <ccbaquero@yahoo.com>
2019-09-30 17:43:49 -0500

    Added new code for arm and made a private for intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -1,47 +1,50 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Teleop", group = "Teleop" )
-public class TeleopMode extends LinearOpMode
+public class TeleopMode extends OpMode
 {
     //Declare motors and stuff
 
     private DcMotor motorLeft;
     private DcMotor motorRight;
     private DcMotor motorArm;
-    double armPower = 1;
+    private DcMotor motorIntake;
+    private final double MAXINTAKESPEED = 1;
 
     @Override
-    public void runOpMode() throws InterruptedException
+    public void init() {
+        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+        motorArm = hardwareMap.get(DcMotor.class, "motorArm");
+        motorIntake = hardwareMap.get(DcMotor.class, "motorIntake");
+
+    }
+
+    @Override
+    public void loop()
     {
-        motorLeft = hardwareMap.dcMotor.get("motorLeft");
-        motorRight = hardwareMap.dcMotor.get("motorRight");
-        motorArm = hardwareMap.dcMotor.get("motorArm");
-
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
-        waitForStart();
-
-        while(opModeIsActive())
+        motorLeft.setPower(gamepad1.left_stick_y);
+        motorRight.setPower(gamepad1.right_stick_y);
+        if(gamepad1.left_bumper)
         {
-            motorLeft.setPower(-gamepad1.left_stick_y);
-            motorRight.setPower(-gamepad1.right_stick_y);
-            if(gamepad1.left_bumper)
-            {
-                motorArm.setPower(-armPower);
-                telemetry.addData("armon", 1);
-                telemetry.update();
-            }
+            motorArm.setPower(-MAXINTAKESPEED);
+            motorIntake.setPower(MAXINTAKESPEED);
 
-            else if (gamepad1.right_bumper)
-            {
-                motorArm.setPower(armPower);
-            }
-            idle(); //gives software a chance to catch up?
-            telemetry.addData("Robot On", 1);
-            telemetry.update();
+        } else if (gamepad1.right_bumper)
+        {
+            motorArm.setPower(MAXINTAKESPEED);
+            motorIntake.setPower(-MAXINTAKESPEED);
         }
+        else
+        {
+            motorArm.setPower(0);
+        }
+
+        
     }
 }
