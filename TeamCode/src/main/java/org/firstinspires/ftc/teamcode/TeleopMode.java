58eda5d426e80575e9eaf1a13f778628613bae9a
Rene-Baquero <ccbaquero@yahoo.com>
2019-10-15 18:27:41 -0500

    This is the epic crab drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -10,41 +10,65 @@ public class TeleopMode extends OpMode
 {
     //Declare motors and stuff
 
-    private DcMotor motorLeft;
-    private DcMotor motorRight;
-    private DcMotor motorArm;
-    private DcMotor motorIntake;
+    private DcMotor motorFrontRight;
+    private DcMotor motorFrontLeft;
+    private DcMotor motorBackLeft;
+    private DcMotor motorBackRight;
+    //private DcMotor motorArm;
+    //private DcMotor motorIntake;
     private final double MAXINTAKESPEED = 1;
 
     @Override
     public void init() {
-        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
-        motorArm = hardwareMap.get(DcMotor.class, "motorArm");
-        motorIntake = hardwareMap.get(DcMotor.class, "motorIntake");
+        motorFrontLeft = hardwareMap.get(DcMotor.class, "motorFrontLeft");
+        motorFrontRight = hardwareMap.get(DcMotor.class, "motorFrontRight");
+        motorBackLeft = hardwareMap.get(DcMotor.class, "motorBackLeft");
+        motorBackRight = hardwareMap.get(DcMotor.class, "motorBackRight");
+        //motorArm = hardwareMap.get(DcMotor.class, "motorArm");
+        //motorIntake = hardwareMap.get(DcMotor.class, "motorIntake");
 
     }
 
     @Override
     public void loop()
     {
-        motorLeft.setPower(gamepad1.left_stick_y);
-        motorRight.setPower(gamepad1.right_stick_y);
+
+        motorBackRight.setPower(-gamepad1.left_stick_y);//forward and backward
+        motorBackLeft.setPower(gamepad1.left_stick_y);
+        motorFrontLeft.setPower(gamepad1.left_stick_y);
+        motorFrontRight.setPower(-gamepad1.left_stick_y);
+
+        motorBackRight.setPower(-gamepad1.right_stick_x);//turning
+        motorBackLeft.setPower(-gamepad1.right_stick_x);
+        motorFrontLeft.setPower(-gamepad1.right_stick_x);
+        motorFrontRight.setPower(-gamepad1.right_stick_x);
+
+        motorBackRight.setPower(-gamepad1.left_trigger);//strafe left
+        motorBackLeft.setPower(-gamepad1.left_trigger);
+        motorFrontLeft.setPower(gamepad1.left_trigger);
+        motorFrontRight.setPower(gamepad1.left_trigger);
+
+        motorBackRight.setPower(gamepad1.right_trigger);//strafe right
+        motorBackLeft.setPower(gamepad1.right_trigger);
+        motorFrontLeft.setPower(-gamepad1.right_trigger);
+        motorFrontRight.setPower(-gamepad1.right_trigger);
+
+
         if(gamepad1.left_bumper)
         {
-            motorArm.setPower(-MAXINTAKESPEED);
-            motorIntake.setPower(MAXINTAKESPEED);
+            //motorArm.setPower(-MAXINTAKESPEED);
+            //motorIntake.setPower(MAXINTAKESPEED);
 
         } else if (gamepad1.right_bumper)
         {
-            motorArm.setPower(MAXINTAKESPEED);
-            motorIntake.setPower(-MAXINTAKESPEED);
+            //motorArm.setPower(MAXINTAKESPEED);
+            //motorIntake.setPower(MAXINTAKESPEED);
         }
         else
         {
-            motorArm.setPower(0);
+            //motorArm.setPower(0);
         }
 
-        
+
     }
 }
